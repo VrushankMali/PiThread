6423a10729d46cf48d9f6b5e2c0d53d29827594b
VrushankMali <VrushankMali@github.com>
2021-10-14 16:38:31 -0500

    Add project files.

---
+++ PIThreading/FindPiThread.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PIThreading
+{
+    class FindPiThread
+    {
+        int totalDarts, circleDarts;
+        Random ran;
+
+        public FindPiThread(int nDarts)
+        {
+            ran = new Random();
+            totalDarts = nDarts;
+            circleDarts = 0;
+        }
+
+        public int getDarts()
+        {
+            return circleDarts;
+        }
+
+        public void throwDarts()
+        {
+            double x, y;
+
+            for(int i=0; i<totalDarts; i++)
+            {
+                x = ran.NextDouble();
+                y = ran.NextDouble();
+
+                if(Math.Sqrt((x*x) + (y*y)) < 0.5)
+                {
+                    circleDarts++;
+                }
+            }
+        }
+    }
+}
---
+++ PIThreading/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace PIThreading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cDarts = 0;
+
+            Console.WriteLine("How many threads you want to throw: ");
+            string thread = Console.ReadLine();
+            int nThread = Convert.ToInt32(thread);
+
+            Console.WriteLine("How many darts in each thread: ");
+            string dart = Console.ReadLine();
+            int nDart = Convert.ToInt32(dart);
+
+            List<FindPiThread> pi = new List<FindPiThread>();
+            List<Thread> thrd = new List<Thread>();
+
+            for (int i = 0; i < nThread; i++)
+            {
+                FindPiThread FPI = new FindPiThread(nDart);
+                pi.Add(FPI);
+
+                Thread th = new Thread(new ThreadStart(FPI.throwDarts));
+                thrd.Add(th);
+
+                th.Start();
+                Thread.Sleep(30);
+            }
+
+            foreach (Thread items in thrd)
+            {
+                items.Join();
+            }
+
+            foreach (FindPiThread piThread in pi)
+            {
+                cDarts += piThread.getDarts();
+            }
+
+            Console.WriteLine(4.0 * (Convert.ToDouble(cDarts) / Convert.ToDouble(nThread * nDart)));
+            Console.ReadLine();
+        }
+    }
+}
